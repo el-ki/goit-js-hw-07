3e3839d13592812c59cea9c1c284545d740f1ecd
Elena Kichinskaia <nitress@gmail.com>
2021-01-25 19:47:03 +0200

    task-7 completed

--- js/task-7.js
+++ js/task-7.js
@@ -5,3 +5,13 @@
 // <input id="font-size-control" type="range" />
 // <br />
 // <span id="text">Абракадабра!</span>
+
+const inputRef = document.querySelector('#font-size-control')
+const spanRef = document.querySelector('#text')
+
+console.log(inputRef.value);
+
+inputRef.addEventListener('input', event => { 
+    const value = event.target.value
+        spanRef.style.fontSize = `${value/3}px`
+})
